29b978eedcb1268237c953cc0bcf5f02b55636d1
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-14 10:12:12 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,12 +16,12 @@ class BankAccount
     "Your balance is $#{balance}."
   end
 
-  def close_account
-    self.status = "closed"
-  end
-
   def valid?
     balance > 0 && status == "open"
+  end
+
+  def close_account
+    self.status = "closed"
   end
 
 end
