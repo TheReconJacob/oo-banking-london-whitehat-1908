00335ed7a7f97a9d9d27a4597012dbf9457ebb89
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-14 10:11:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,12 @@ class Transfer
       reject_transfer
     end
   end
-
+  
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+  
   def reverse_transfer
     if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -= amount
